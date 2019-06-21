c8d9cb21c7831329de304b49e15176ffad7c3832
Ralph <ralphboot@gmail.com>
2019-06-21 00:07:58 -0300

    Create Reduce.py
    Função Reduce

---
+++ Reduce.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-'''
+
+import sys
+
+ocorrencia = {}
+x = 0
+
+for linha in sys.stdin:
+	#lê a chave e valor gerada pelo map
+	chave,valor = linha.split("\t", 1)
+
+	try:
+		valor = int(valor)
+
+	except ValueError:
+		continue;
+
+	try:
+		ocorrencia[chave] = ocorrencia[chave] + valor
+	except:
+		ocorrencia[chave] = valor
+#Fiz um cantador para cada de tweet apartir do id_tweet registrado por determinada empresa.
+for id_tweet in ocorrencia.keys():
+	x += 1
+	#print '%s' % (ocorrencia[id_autor])
+print 'Foram respondidos:' , x, 'Tweets'
+	
